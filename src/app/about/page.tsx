d65c4a5a105735cdb72ca0e21b198764daa03fc7
“[firstname <[gmugsk@gmail.com]>
2024-10-09 13:26:19 +0500

    Rout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+   return(
+     <div>
+       <h1>This is a About page from <strong> Ghulam Muneer Uddin</strong></h1>
+       <p><span>This is a next.js 2nd project</span></p>
+     </div>
+   );
+ }
---
+++ src/app/header/page.tsx
@@ -0,0 +1,8 @@
+export default function Header(){
+   return(
+     <div>
+       <h1>This is a Header page from <strong> Ghulam Muneer Uddin</strong></h1>
+       <p><span>This is a next.js 2nd project</span></p>
+     </div>
+   );
+ }
